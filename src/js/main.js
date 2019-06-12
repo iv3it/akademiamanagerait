48aebcfc095ecce00b66f50916ee208620c45761
Kinga <41168092+iv3it@users.noreply.github.com>
2019-06-12 15:42:59 +0200

    Added mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,42 @@ $(document).on("click", 'a[href^="#"]', function(event) {
     700
   );
 });
+
+//Hamburger
+
+let menu = $(".navigation_mobile");
+let hamb = $(".hamburger_menu");
+let body = $("body");
+
+hamb.click(function() {
+  menu.toggleClass("open");
+  body.toggleClass("menu_open");
+
+  if (hamb.hasClass("close_menu")) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+$(window).resize(function() {
+  let curWidth = $(window).width();
+
+  if (curWidth >= 1550) {
+    closeMenu();
+  }
+});
+
+$(".navigation_mobile li").click(function() {
+  closeMenu();
+});
+
+function openMenu() {
+  hamb.addClass("close_menu");
+}
+
+function closeMenu() {
+  body.removeClass("menu_open");
+  menu.removeClass("open");
+  hamb.removeClass("close_menu");
+}
