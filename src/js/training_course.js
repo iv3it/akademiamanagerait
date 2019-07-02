d69886d5ed161f8b091ddfb7f894907e6d6f1bd4
Kinga <41168092+iv3it@users.noreply.github.com>
2019-07-02 12:36:41 +0200

    update

--- src/js/training_course.js
+++ src/js/training_course.js
@@ -9,13 +9,26 @@ window.addEventListener("DOMContentLoaded", event => {
 $("#description_btn").on("click", function() {
   $("#programme_description_holder").removeClass("hide");
   $("#programme_holder").addClass("hide");
+  $("#instructor_holder").addClass("hide");
   $("#description_btn").addClass("underline");
   $("#programme_btn").removeClass("underline");
+  $("#instructor_btn").removeClass("underline");
 });
 
 $("#programme_btn").on("click", function() {
-  $("#programme_description_holder").addClass("hide");
   $("#programme_holder").removeClass("hide");
+  $("#programme_description_holder").addClass("hide");
+  $("#instructor_holder").addClass("hide");
   $("#programme_btn").addClass("underline");
   $("#description_btn").removeClass("underline");
+  $("#instructor_btn").removeClass("underline");
+});
+
+$("#instructor_btn").on("click", function() {
+  $("#instructor_holder").removeClass("hide");
+  $("#programme_description_holder").addClass("hide");
+  $("#programme_holder").addClass("hide");
+  $("#instructor_btn").addClass("underline");
+  $("#description_btn").removeClass("underline");
+  $("#programme_btn").removeClass("underline");
 });
